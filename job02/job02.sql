418cc759d57f2198b089df62bacdd5a8be7f56fa
Adrien Dirix <adrien.dirix@laplateforme.io>
2022-11-09 23:52:07 +0100

    job02

---
+++ job02/job02.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: jobsql
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `etage`
+--
+
+DROP TABLE IF EXISTS `etage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `etage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `numero` int DEFAULT NULL,
+  `superficie` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etage`
+--
+
+LOCK TABLES `etage` WRITE;
+/*!40000 ALTER TABLE `etage` DISABLE KEYS */;
+INSERT INTO `etage` VALUES (1,'RDC',0,500),(2,'R+1',1,500),(3,'R+2',2,500);
+/*!40000 ALTER TABLE `etage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etudiants`
+--
+
+DROP TABLE IF EXISTS `etudiants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `etudiants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `naissance` date DEFAULT NULL,
+  `sexe` varchar(25) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etudiants`
+--
+
+LOCK TABLES `etudiants` WRITE;
+/*!40000 ALTER TABLE `etudiants` DISABLE KEYS */;
+INSERT INTO `etudiants` VALUES (1,'Cyril','Zimmermann','1989-01-02','Homme','cyril@laplateforme.io'),(2,'Jessica','Soriano','1995-09-08','Femme','jessica@laplateforme.io'),(3,'Roxan','Roumégas','2016-09-08','Homme','roxan@laplateforme.io'),(4,'Pascal','Assens','1999-12-31','Homme','pascal@laplateforme.io'),(5,'Terry','Cristinelli','2005-02-01','Homme','terry@laplateforme.io'),(6,'Ruben','Habib','1993-05-26','Homme','ruben.habib@laplateforme.io'),(7,'Toto','Dupont','2019-11-07','Homme','toto.dupont@laplateforme.io');
+/*!40000 ALTER TABLE `etudiants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salles`
+--
+
+DROP TABLE IF EXISTS `salles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `id_etage` int DEFAULT NULL,
+  `capacite` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salles`
+--
+
+LOCK TABLES `salles` WRITE;
+/*!40000 ALTER TABLE `salles` DISABLE KEYS */;
+INSERT INTO `salles` VALUES (1,'Lounge',1,100),(2,'Studio Son',1,5),(3,'Projection',2,50),(4,'Bocal Peda',2,3),(5,'Coworking',2,80),(6,'Studio Video',2,5),(7,'C++',3,8),(8,'C',3,6),(9,'Big Black Room',3,80),(10,'New Double',3,25),(11,'Double',3,25),(12,'Wesh Grow',3,1),(13,'Front room',3,20),(14,'Back room',3,20);
+/*!40000 ALTER TABLE `salles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-09 23:29:23
